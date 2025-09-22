6085f1296a96a8bef2e20ecff674b5dbf4ca3935
ganeshgoud43 <ganeshgoud43@gmail.com>
2025-09-22 15:09:07 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,9 +1,10 @@
 def gitDownload(repo)
 {
- git "https://github.com/ganeshgoud43/${repo}.git"   
+    git "https://github.com/ganeshgoud43/${repo}.git"
 }
 
-def Build()
+def buildArtifact()
 {
-  sh 'mvn package'
+     sh 'mvn package'
+
 }
